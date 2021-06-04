0ab24cb6d8389c7fcb90000a3006e70cde2bef32
Bitpaint <67663265+bitpaint@users.noreply.github.com>
2021-06-04 15:43:52 +0200

    Update @gource.sh

--- gource/@gource.sh
+++ gource/@gource.sh
@@ -1,1 +1,1 @@
-gource combined.txt -1920x1080 -r 60 --seconds-per-day 0.02 --file-idle-time 0 --date-format "%B %Y" --hide filenames,users,dirnames --caption-file caption.txt --caption-size 24 --caption-duration 5 --user-image-dir avatar/ 
+gource combined.txt -1920x1080 -r 60 --seconds-per-day 0.02 --date-format "%B %Y" --hide filenames,users,dirnames --caption-file caption.txt --caption-size 24 --caption-duration 5 --user-image-dir avatar/ 
