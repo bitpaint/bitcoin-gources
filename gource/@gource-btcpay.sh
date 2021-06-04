ed509c99425216cbc9f25a8d1774b063ee257a31
Bitpaint <67663265+bitpaint@users.noreply.github.com>
2021-06-04 15:44:04 +0200

    Update @gource-btcpay.sh

--- gource/@gource-btcpay.sh
+++ gource/@gource-btcpay.sh
@@ -1,1 +1,1 @@
-gource btcpay.txt -1920x1080 -r 60 --seconds-per-day 0.02 --file-idle-time 0 --date-format "%B %Y" --hide filenames,dirnames   --user-image-dir avatar/ 
+gource btcpay.txt -1920x1080 -r 60 --seconds-per-day 0.02 --date-format "%B %Y" --hide filenames,dirnames   --user-image-dir avatar/ 
